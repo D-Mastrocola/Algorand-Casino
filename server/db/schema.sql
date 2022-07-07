0a6e4b5ccd8390a4eb90c5500878cfb502bc3e72
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2022-07-06 22:41:56 -0400

    boilerplate for the backend routes, models and database

---
+++ server/db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS test_nft_db;
+
+CREATE DATABASE test_nft_db;
--- server/server.js
+++ server/server.js
@@ -1,33 +1,17 @@
-//npm i express
 const express = require('express');
-
-const path = require('path');
-
-/*const routes = require('./controllers');
-
-//npm i sequelize
-//const sequelize = require('./config/connection');
-// npm install --save path
-const path = require('path');
-// npm i express-session connect-session-sequelize
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const helpers = require('./utils/helpers');
-
-
-
-// turn on connection to db and server
-// force true will always sync to server (reset) upon npm start
-sequelize.sync({ force: false }).then(() => {
-  
-});*/
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log('Now listening'));
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
