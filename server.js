36fa8c5c37f8af7b226b755b6d891b0e60ac0698
D-Mastrocola <mastrodomcola@gmail.com>
2023-01-22 16:33:47 -0500

    added partials and helper functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -32,7 +33,7 @@ app.use(routes);
 
 //Handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
