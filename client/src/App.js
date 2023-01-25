b3c82f7dab29b2f83b98dd40e2ca573351020b7a
D-Mastrocola <mastrodomcola@gmail.com>
2023-01-25 11:02:57 -0500

    react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './pages/components/header/Header';
-import Home from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./pages/components/header/Header";
+import Home from "./pages/Home";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home />
-    </div>
+    <Router>
+      <Header />
+
+      <Route exact path='/'>
+        <Home />
+      </Route>
+    </Router>
   );
 }
 
--- client/src/pages/components/artwork/Artwork.jsx
+++ client/src/pages/components/artwork/Artwork.jsx
@@ -5,15 +5,19 @@ class Artwork extends Component {
     super(props);
     this.state = {
       connected: "Connect Wallet",
+      id: this.props.id
     };
   }
   render() {
+    console.log(this.state.id)
     return (
       <article className="col m-3">
+        <a href={"/posts/" + this.state.id}>
         <img src="/images/test_nft.jfif" style={{ maxWidth: "100%" }} />
         <h3>
           <a>Test NFT</a>
         </h3>
+        </a>
       </article>
     );
   }
